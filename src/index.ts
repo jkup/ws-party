223420544b77d79e98695274379fcd897016eb23
Jon Kuperman <jon.kuperman@gmail.com>
2022-09-28 19:11:41 +0100

    I dont even know

--- src/index.ts
+++ src/index.ts
@@ -27,5 +27,31 @@ export default {
 };
 
 export class WebSocketDurableObject {
-  constructor() {}
+  state: DurableObjectState;
+  constructor(state: DurableObjectState, env: any) {
+    this.state = state;
+  }
+
+  async fetch(request: Request) {
+    // To accept the WebSocket request, we create a WebSocketPair (which is like a socketpair,
+    // i.e. two WebSockets that talk to each other), we return one end of the pair in the
+    // response, and we operate on the other end. Note that this API is not part of the
+    // Fetch API standard; unfortunately, the Fetch API / Service Workers specs do not define
+    // any way to act as a WebSocket server today.
+    let pair = new WebSocketPair();
+
+    // We're going to take pair[1] as our end, and return pair[0] to the client.
+    await this.handleSession(pair[1]);
+
+    // Now we return the other end of the pair to the client.
+    return new Response(null, { status: 101, webSocket: pair[0] });
+  }
+
+  async handleSession(websocket: WebSocket) {
+    websocket.accept();
+
+    websocket.addEventListener("message", async (msg) => {
+      console.log(msg);
+    });
+  }
 }
